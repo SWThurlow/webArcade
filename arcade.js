9bf055c60fb9003bf4e9f0c110d5fd25ff35a953
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-02-07 19:33:23 +0000

    Div element added in js to put game selection     screen elements in to make layout easier. Layout for the game selection screen sorted.     Styling for components still needed. Two place holder images added to test game selection functionality     when it is made.

--- arcade.js
+++ arcade.js
@@ -1,24 +1,33 @@
 /*Get arcade div to load everything in.*/
 let arcade = document.getElementById('arcade');
 
+/*The Games*/
+/*
+let games = [
+    Need to create objects with thumbnails, titles and function for each game.
+];
+*/
 /*Game selection*/
+let selectionScreen = document.createElement('div');
+selectionScreen.setAttribute('class', 'selection');
+arcade.appendChild(selectionScreen);
 let gameTitle = document.createElement('h3');
 gameTitle.setAttribute('class', 'title');
 gameTitle.innerHTML = 'Game Title';
-arcade.appendChild(gameTitle);
+selectionScreen.appendChild(gameTitle);
 let prevBtn = document.createElement('btn');
 prevBtn.setAttribute('class', 'previous');
 prevBtn.innerHTML = 'Previous';
-arcade.appendChild(prevBtn);
+selectionScreen.appendChild(prevBtn);
 let gameThumb = document.createElement('img');
 gameThumb.setAttribute('class', 'thumb');
 gameThumb.setAttribute('src', '#');
-arcade.appendChild(gameThumb);
+selectionScreen.appendChild(gameThumb);
 let nextBtn = document.createElement('btn');
 nextBtn.setAttribute('class', 'next');
 nextBtn.innerHTML = 'Next';
-arcade.appendChild(nextBtn);
+selectionScreen.appendChild(nextBtn);
 let play = document.createElement('btn');
 play.setAttribute('class', 'play');
 play.innerHTML = 'Play';
-arcade.appendChild(play);
+selectionScreen.appendChild(play);
